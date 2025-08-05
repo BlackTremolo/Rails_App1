fb79da8811d2853c80869091fc4752ff6547d635
Mihser <mihsee436@gmail.com>
2025-08-05 19:56:05 +0800

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,12 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contacts = Contact.new(contact_params)
+    @contacts.save
   end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:email, :message)
+    end
 end
