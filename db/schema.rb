d55bc7d7ce8311ec0cc2f9e2286f765cb700b4df
Mihser <mihsee436@gmail.com>
2025-08-05 19:20:40 +0800

    Add contact model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_04_124134) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_05_092125) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
